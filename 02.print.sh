fd36c149edd92c36524853f5db81bf60dc2cb271
udayayeswari <udayeswari.savana@gmail.co>
2023-02-19 08:46:15 -0500

    shell scripting topics

---
+++ 02.print.sh
@@ -0,0 +1,14 @@
+#learning-shell
+### list of shell scripting topics
+
+
+1. shebang & comments
+2. print                         (command)
+3. redirectors & qoutes          (linux concepts)
+4. variables                     (scripting concepts)
+5. Input                         (scripting concepts)
+6. functions                     (scripting concepts)
+7. exit status                   (command)
+8. conditiond                    (scripting concepts)
+9. loops                         (scripting concepts)
+10. SED editor                    (command )
